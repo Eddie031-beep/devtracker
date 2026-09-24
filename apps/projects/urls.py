399821f27dd137ef8c8567bf22ebe006fdc55782
Eddie Man <eddiejrman@gmail.com>
2026-09-24 03:03:48 -0500

    Agrega endpoints para entregar y revisar entregables

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from . import views, views_workflow
 
 urlpatterns = [
     path("projects/", views.ProjectListCreateView.as_view(), name="project_list_create"),
@@ -9,4 +9,6 @@ urlpatterns = [
     path("milestones/<int:pk>/", views.MilestoneDetailView.as_view(), name="milestone_detail"),
     path("deliverables/", views.DeliverableListCreateView.as_view(), name="deliverable_list_create"),
     path("deliverables/<int:pk>/", views.DeliverableDetailView.as_view(), name="deliverable_detail"),
+        path("deliverables/<int:pk>/submit/", views_workflow.DeliverableSubmitView.as_view(), name="deliverable_submit"),
+    path("deliverables/<int:pk>/status/", views_workflow.DeliverableStatusView.as_view(), name="deliverable_status"),
 ]
